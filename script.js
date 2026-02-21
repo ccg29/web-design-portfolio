3f95a9aa61b6f4a3ebbc8fc018160bd95d4f6938
Camila Conchas-Garcia <camila.conchas-garcia@du.edu>
2026-02-21 08:50:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let username; 
-document.getElementById('mySubmit').onclick = function(){
-  username = document.getElementById('myText').value; 
-  document.getElementById('myH1').textContent = 'Hi there ${username}!'
+document.querySelector('button').onclick = function(){
+  username = document.querySelector('input').value; 
+  document.querySelector('H1').textContent = 'Hi there ${username}!'
 }
