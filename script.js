921781236d148df89f4f1aa9ecc513fd68eea7d8
Camila Conchas-Garcia <camila.conchas-garcia@du.edu>
2026-02-20 22:15:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let username; 
 document.GetElementByID("mySubmit").onclick = function(){
-  username = document.getElementByID("myText).value; 
+  username = document.getElementByID("myText").value; 
   document.getElementByID("myH1").textContent = 'Hi there ${username}!'
 }
