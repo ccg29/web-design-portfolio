f8e7cd5e8a6d7c30c86f0a4666c14dd74dfcfad4
Camila Conchas-Garcia <camila.conchas-garcia@du.edu>
2026-02-20 22:04:54 -0700

    Update script.js
    Adding Javascript Username interaction

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+let username; 
+document.GetElementByID("mySubmit").onclick = function(){
+  username = document.getElementByID("myText).value; 
+  document.getElementByID("myH1").textContent = 'Hi there ${username}!'
+}
