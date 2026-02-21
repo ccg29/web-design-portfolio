497d5b598703dd818b61e044244050c65a1737e3
Camila Conchas-Garcia <camila.conchas-garcia@du.edu>
2026-02-21 09:28:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+document.title = "Mini-Demo| Cami's Corner";
+
+const welcomeMsg = document.getElementById("myH1");
+
+const useranme = document.getElementById ('myusername");
+
 let username; 
 document.querySelector('button').onclick = function(){
   username = document.querySelector('input').value; 
