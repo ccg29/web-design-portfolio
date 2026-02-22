62d0b947c4642f8ac6112ccdce396ccd85db886b
Camila Conchas-Garcia <camila.conchas-garcia@du.edu>
2026-02-21 22:10:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const readMoreBttn = document.querySelectorAll('.more-text-bttn');
-const text = document.querySelectorAll('.text'); 
+const readMoreBttn = document.querySelector('.more-text-bttn');
+const text = document.querySelector('.text'); 
 
 readMoreBttn.addEventListener('click',(e)=>{
   text.classList.toggle('show-more');
