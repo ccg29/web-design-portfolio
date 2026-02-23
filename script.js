f547cdca7ba9e50b0839d78ef07c694141f058ff
Camila Conchas-Garcia <camila.conchas-garcia@du.edu>
2026-02-22 20:31:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const readMoreBttn = document.querySelectorAll('.more-text-bttn');
-const text = document.querySelectorAll('.text'); 
+const text = document.querySelectorAll('.text');
 
-readMoreBttn.forEach((readMoreBttn, index) => {
-readMoreBttn.addEventListener('click'), (e)=>{
-  if (texts[index]){texts[index].classList.toggle('show');
-  }
-  if(readMoreBttn.innerText === 'Read-More'){
-    readMoreBttn.innerText = 'Read Less';
-  }else{
-    readMoreBttn.innerText = 'Read More';
-  }
-}})
+readMoreBttn.forEach((button, index) => {
+  button.addEventListener('click', (e) => {
+    if (text[index]) {
+      text[index].classList.toggle('show');
+    }
+    if (button.innerText === 'Read-More') {
+      button.innerText = 'Read Less';
+    } else {
+      button.innerText = 'Read More';
+    }
+  });
+});
