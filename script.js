26f69288df3f06ae7f80be5e01c8c10ebcd5864a
Camila Conchas-Garcia <camila.conchas-garcia@du.edu>
2026-02-22 21:01:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const text = document.querySelectorAll('.text');
 readMoreBttn.forEach((button, index) => {
   button.addEventListener('click', (e) => {
     if (text[index]) {
-      text[index].classList.toggle('show. moreText');
+      text[index].classList.toggle('show');
     }
     if (button.innerText === 'Read More') {
       button.innerText = 'Read Less';
